2bfad64c4b948a92e9353dad36e7650689f85d76
Faye Carter <fayecarter333@gmail.com>
2020-03-12 13:30:52 +0000

    Creation of first rspec test

---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,11 @@
+require 'roman_numerals'
+
+describe RomanNumerals do
+  it "should be able to convert 1 to I" do
+    expect(RomanNumerals.convert(1)).to eq("I")
+  end
+
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+end
